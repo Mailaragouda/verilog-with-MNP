4e297f315e1aba0915e8969854aac18e875cdb0c
Mailaragouda N  P <mailaragoudanp@gmail.com>
2025-06-28 21:16:37 +0530

    Delete digital_design_using_verilog/substractors directory

--- digital_design_using_verilog/substractors/HA_dataflow.v
+++
@@ -1,9 +0,0 @@
-//HA using dataflow modelling
-module HA(a,b,diff,barrow);
-  input a,b;
-  output diff,barrow;
-  
-  assign diff=a^b;   //or assign sum=(~a&b)|(a&~b);
-  assign barrow=~a&b;  
-  
-endmodule
